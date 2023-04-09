caef56672d45c285b175ee1cedfc370ebfb9a58a
jimmypvu <114040438+jimmypvu@users.noreply.github.com>
2023-04-09 15:40:14 -0700

    minor refactor

--- src/main/java/org/framework/Main.java
+++ src/main/java/org/framework/Main.java
@@ -1,7 +1,7 @@
-package org.example;
+package org.framework;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello framework!");
     }
 }
