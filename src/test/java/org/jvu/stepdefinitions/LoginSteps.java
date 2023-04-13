72913e516913ab4bd9eb8526c69959a863c8b986
jimmypvu <114040438+jimmypvu@users.noreply.github.com>
2023-04-13 11:06:12 -0700

    updated login step defs

--- src/test/java/org/jvu/stepdefinitions/LoginSteps.java
+++ src/test/java/org/jvu/stepdefinitions/LoginSteps.java
@@ -18,20 +18,23 @@ public class LoginSteps {
         HomePage hp = new HomePage(getCucumberDriver());
         hp.clickLoginBtn();
     }
+
     @When("^user enters username and password$")
     public void iEnterUsernameAndPassword() {
         LoginPage lp = new LoginPage(getCucumberDriver());
         lp.setUsername("sirtestsalot").setPassword("1234iTestMore!");
     }
+
     @And("user clicks Login button")
     public void iClickLogin() {
         LoginPage lp = new LoginPage(getCucumberDriver());
         lp.clickLoginBtn();
     }
+
     @Then("^user is logged into account page$")
     public void iAmLoggedIn() {
         AccountPage ap = new AccountPage(getCucumberDriver());
-        WebElement myAccHdr = ap.waitForPresence(AccountPage.byMyAccountHdr);
+        WebElement myAccHdr = ap.waitForPresAndGet(AccountPage.byMyAccountHdr);
 
         Assert.assertTrue(myAccHdr.isDisplayed());
         Assert.assertEquals(getCucumberDriver().getCurrentUrl(), "https://automationteststore.com/index.php?rt=account/account");
@@ -40,7 +43,7 @@ public class LoginSteps {
     @Then("^user is not logged in$")
     public void iAmNotLoggedIn() {
         LoginPage lp = new LoginPage(getCucumberDriver());
-        WebElement error = lp.waitForPresence(LoginPage.byLoginErrorLbl);
+        WebElement error = lp.waitForPresAndGet(LoginPage.byLoginErrorLbl);
 
         Assert.assertTrue(error.isDisplayed());
         Assert.assertEquals(getCucumberDriver().getCurrentUrl(), "https://automationteststore.com/index.php?rt=account/login");
